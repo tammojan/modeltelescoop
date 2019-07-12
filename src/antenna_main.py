fba306a94388753e4062f71854fe283ea1d08fc7
Tammo Jan Dijkema <dijkema@astron.nl>
2019-07-12 13:19:08 +0200

    Try to reset socket after IOError

--- src/antenna_main.py
+++ src/antenna_main.py
@@ -15,10 +15,7 @@ def send_altaz(sock: socket, alt: float, az: float):
     """ Send an altaz pair to the connected server through the socket"""
     ob = {'alt': round(alt), 'az': round(az)}
     print(ob)
-    try:
-        sock.send(bytes(json.dumps(ob), 'utf-8'))
-    except IOError:
-        print('Error: Broken pipe')
+    sock.send(bytes(json.dumps(ob), 'utf-8'))
 
 
 def convert_azimuth(az_in: float):
@@ -54,32 +51,39 @@ def main():
     sensor_alt.enable()
 
     # Create the client socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = None
 
     # Connect to the server via port 7272, with 50 tries
     counter = 1
-    while counter <= 50:
+    while True:
+      if s:
+          print("Socket exists, closing")
+          s.close()
+      else:
+          s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+      try:
         try:
+            print("Trying to connect")
             s.connect(('192.168.4.1', 7272))
-            break
         except OSError as e:
             # We got an error, print out what, then sleep and try again
             print("Connection failed (attempt {}): {}".format(counter, e))
             sleep(5)
             counter = counter + 1
             continue
-    with s:
-        print('Connected')
-        try:
-            while True:
-                print('Transmitting')
-                azimuth = convert_azimuth(sensor_azi.get_angle())
-                altitude = convert_altitude(sensor_alt.get_angle())
-                print(azimuth)
-                send_altaz(s, altitude, azimuth)
-                sleep(0.2)
-        except KeyboardInterrupt:
-            print('Quitting...')
+        with s:
+          print('Connected')
+          while True:
+              azimuth = convert_azimuth(sensor_azi.get_angle())
+              altitude = convert_altitude(sensor_alt.get_angle())
+              send_altaz(s, altitude, azimuth)
+              sleep(0.2)
+      except IOError:
+          print("Broken pipe, reconnecting")
+      except KeyboardInterrupt:
+          print('Quitting...')
+          break
 
     sensor_azi.finish()
     sensor_alt.finish()
