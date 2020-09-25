caa1519d938017c0b49e95f94f5ac2e06a056013
Stefan van der Linden <spvdlinden@gmail.com>
2020-09-25 11:10:59 +0200

    Added fallbacks for yaml loading

--- src/renderbar.py
+++ src/renderbar.py
@@ -14,6 +14,14 @@ import yaml
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0, 255)
 
+
+class Bar:
+    def __init__(self, image, sound):
+        self.image = image
+        self.preloaded_image = preload_image(image)
+        self.sound = sound
+
+
 def preload_image(image_path):
     return pygame.image.load(image_path).convert()
 
@@ -21,12 +29,12 @@ class RenderBar(RenderBase):
     def __init__(self):
         self.bar_mode = None
         self.bars = {}
-        self.bars["De Melkweg"] = preload_image('./resources/bar_milkyway.png')
-        self.bars[None] = preload_image('./resources/bar_empty.png')
+        self.bars["De Melkweg"] = Bar('./resources/bar_milkyway.png', '')
+        self.bars[None] = Bar('./resources/bar_empty.png', '')
 
         bodies_yaml = yaml.load(open("bodies.yml", "r"))
         for body in bodies_yaml:
-            self.bars[body["title"]] = preload_image("./resources/" + body["bar_image"])
+            self.bars[body["title"]] = Bar("./resources/" + body.get("bar_image", "bar_empty.png"), body.get("sound", ""))
         
         self.full_init = True
         self.changed_mode = True
@@ -38,11 +46,11 @@ class RenderBar(RenderBase):
         if self.full_init:
             rects_to_update = screen.fill(BLACK, pygame.Rect(1100, 0, 820, 1080))
 
-        screen.blit(self.bars[self.bar_mode], (1082, 0))
+        screen.blit(self.bars[self.bar_mode].preloaded_image, (1082, 0))
 
         if self.changed_mode:
             rects_to_update = screen.fill(BLACK, pygame.Rect(1100, 200, 820, 1080))
-            screen.blit(self.bars[self.bar_mode], (1082, 0))
+            screen.blit(self.bars[self.bar_mode].preloaded_image, (1082, 0))
 
             self.changed_mode = False
             
